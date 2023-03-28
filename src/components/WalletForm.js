b309dc0997a0584551a8b2b3307fced84d77e0d8
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-03-28 20:16:13 -0300

    requisito 3.2 e 3.2

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,9 +1,55 @@
 import React, { Component } from 'react';
 
 class WalletForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      description: '',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
   render() {
+    const { value, description } = this.state;
     return (
-      <div>WalletForm</div>
+      <>
+        <div>WalletForm</div>
+        <form onSubmit={ this.handleSubmit }>
+          <label>
+            Valor:
+            <input
+              data-testid="value-input"
+              type="number"
+              name="value"
+              value={ value }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <br />
+          <label>
+            Descrição:
+            <input
+              data-testid="description-input"
+              type="text"
+              name="description"
+              value={ description }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <br />
+          <button type="submit">Enviar</button>
+        </form>
+      </>
     );
   }
 }
