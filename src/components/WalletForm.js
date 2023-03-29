a441bf26e864e93ed7516853752545f0c4196f90
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-03-28 23:01:21 -0300

    requisito 3

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -13,12 +13,12 @@ class WalletForm extends Component {
     this.handleChange = this.handleChange.bind(this);
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
+  handleSubmit(e) {
+    e.preventDefault();
   }
 
-  handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value });
   }
 
   render() {
@@ -50,13 +50,31 @@ class WalletForm extends Component {
             />
           </label>
           <br />
-          <select data-testid="currency-input">
+          <label htmlFor="currency">Moeda:</label>
+          <select data-testid="currency-input" id="currency">
             {currencies.map((currency) => (
               <option key={ currency } value={ currency }>
                 {currency}
               </option>
             ))}
           </select>
+          <label htmlFor="method">Metodo de pagamento:</label>
+          <select data-testid="method-input" id="method">
+            <option value="method">Dinheiro</option>
+            <option value="method">Cartão de crédito</option>
+            <option value="method">Cartão de débito</option>
+          </select>
+
+          <label htmlFor="tag">Categoria:</label>
+          <select data-testid="tag-input" id="tag">
+            <option value="tag">Alimentação</option>
+            <option value="tag">Lazer</option>
+            <option value="tag">Trabalho</option>
+            <option value="tag">Transporte</option>
+
+            <option value="tag">Saúde</option>
+          </select>
+
           <button type="submit">Enviar</button>
         </form>
       </>
