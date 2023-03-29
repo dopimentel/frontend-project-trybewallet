e39b452aec74f20d12b995fe3afa537cd8e89326
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-03-28 22:13:34 -0300

    requisito 3.4

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,8 +1,26 @@
 // Coloque aqui suas actions
+const URL = 'https://economia.awesomeapi.com.br/json/all';
 // ACTIONS TYPES
 export const SAVE_EMAIL = 'SAVE_EMAIL';
+export const SAVE_CURRENCIES = 'SAVE_CURRENCIES';
+
 // ACTIONS CREATORS
 export const saveEmail = (email) => ({
   type: SAVE_EMAIL,
   email,
 });
+
+export const saveCurrencies = (currencies) => ({
+  type: SAVE_CURRENCIES,
+  currencies,
+});
+
+export const fetchAwesomeAPI = () => (dispatch) => {
+  fetch(URL)
+    .then((response) => response.json())
+    .then((currencies) => dispatch(saveCurrencies(Object.keys(currencies)
+      .filter((currencie) => currencie !== 'USDT'))));
+};
+
+// Object.entries(currencies)
+//       .map((currencie) => ({ ...currencie }))
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,3 +1,5 @@
+import { SAVE_CURRENCIES } from "../actions";
+
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
 const INITIAL_STATE = { currencies: [],
   expenses: [],
@@ -7,6 +9,11 @@ const INITIAL_STATE = { currencies: [],
 
 const wallet = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+  case SAVE_CURRENCIES:
+    return {
+      ...state,
+      currencies: action.currencies,
+    };
   default:
     return state;
   }
