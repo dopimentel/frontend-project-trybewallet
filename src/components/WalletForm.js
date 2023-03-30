219b9267bcb38d3e9732ed7961505929a92e74fa
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-03-30 00:26:07 -0300

    Requesito 4 done - lint

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { addExpense, fetchExchange } from '../redux/actions';
+import { addExpense } from '../redux/actions';
 
 class WalletForm extends Component {
   constructor(props) {
